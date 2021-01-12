08081551c1b9ffc5de2ca5714bbd9b2d4fbeb0e2
YoonEunSik <developer.lisiver@gmail.com>
2021-01-12 14:54:51 +0900

    1. babel, webpack과 함께 react를 사용하기 위한 세팅
    추가적으로 아래와 같은 패키지들을 설치해준다.
    
    @babel/preset-react : react를 babel과 함께 사용하기 위해서 필요한 프리셋
    html-loader : 웹팩이 html을 읽을 수 있게 해주는 로더
    html-webpack-plugin : 템플릿을 지정하거나 favicon을 설정할 수 있게 해주는 플러그인
    webpack-dev-server : 개발환경에 편하도록 웹팩에서 개발서버를 제공하는 패키지
    
    위 추가한 패키지들을 사용하기 위해 npm run dev 스크립트를 package.json에 'webpack-dev-server'를 설정해주고, webpack.config.js 를 알맞게 수정한다.
    
    이렇게 세팅을 다 한 다음 실행해보면, Cannot find module 'webpack-cli/bin/config-yargs' 와 같은 오류와 함께 실행되지 않는다.
    
    그 이유는 webpack과 webpack-dev-server의 버전 호환이 되지 않기 때문

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
 module.exports = {
   mode: 'development',
   entry: ['@babel/polyfill', './src/main.js', './src/main.css'],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.[hash].js'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         include: [
           path.resolve(__dirname, 'src')
         ],
@@ -30,13 +34,33 @@ module.exports = {
           'css-loader',
         ],
         exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ],
   devtool: 'inline-source-map',
   resolve: {
     modules: [path.join(__dirname, 'src'), 'node_modules'],
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss', '.json']
   },
+  devServer: {
+    host: 'localhost',
+    port: port,
+    open: false,
+  }
 }
