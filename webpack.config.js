100848739e6a3bc2b40091d7df045f81f4458e89
YoonEunSik <developer.lisiver@gmail.com>
2021-01-12 11:42:20 +0900

    6. 웹팩 설정하기
    웹팩은 의존 관계에 있는 모듈들을 하나의 js 파일로 번들링하는 모듈 번들러이다.
    다수의 js 파일을 하나의 파일로 번들링 하므로, 5번 커밋에서 생성한 html 파일에서와 같이 여러 스크립트를 로드해야하는 번거로움도 사라진다.
    
    webapck v4 이상에선 webpack-cli를 필수로 요구함.
    
    babel-loader는 웹팩이 모듈을 번들링할 때 babel을 사용하여 ES6+ 코드를 ES5 코드로 트랜스파일링 하도록 하는 로더이다.
    
    루트 디렉토리의 webpack.config.js 에서 웹팩의 설정을 할 수 있다.
    - mode: 개발모드 혹은 상용모드로 설정, production, development 혹은 none 설정 (기본값 production), 둘의 큰 차이는 최적화의 정도 차이
    - entry: 웹팩은 의존성 그래프에 따라 모든 파일들을 읽어들이는데, 이를 시작할 포인트를 지정. 배열로 여러개 설정할 수도 있다.
    - output: 컴파일 + 번들링 된 js 파일이 저장될 경로와 이름을 지정해준다.
    - module: 기본적으로 웹팩은 js또는 json파일만 이해하기 때문에, 이를 포함한 이외 확장자를 어떻게 처리할지 룰을 명시한다.
    - plugins:  로더가 각각 파일을 필터링한다면, 플러그인은 최종적으로 번들링된 파일에 1회만 실행됨
    - resolve:  경로나 확장자를 생략 및 처리할 수 있도록 하는 옵션 설정
    
    웹팩 설정을 통해 .js 파일을 babel로 트랜스파일링 하기 때문에 기존 npm run build 스크립트 명령을 babel 에서 webpack으로 바꾸었다.
    
    웹팩 설정을 따라 npm run build(webpack -w)를 명령하여 실행하면 dist폴더에 bundle.js 로 하나의 js 파일이 추출된다. 이 하나의 파일만을 index.html에서 호출하여 실행해보면 정상적으로 로그가 찍히는 것을 볼 수 있다.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [],
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss', '.json']
+  },
+}
